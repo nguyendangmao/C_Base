8cfc1195e74c5b077cfa5a4428a67ef357ec04fb
phamtruong <phamtruong13032001@gmail.com>
2023-02-28 13:42:37 +0700

    bai6

--- Program.cs
+++ Program.cs
@@ -4,30 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace bai7
+namespace bai6
 {
     class Program
     {
-        static void minimaxsum(int[] arr)
-          
+        static void staircase(int n)
         {
-            Array.Sort(arr);
-            long min = arr.Take(4).Sum();
-            long max = arr.Skip(1).Sum();
-            Console.WriteLine("sum_min:" + min);
-            Console.WriteLine("sum_max:" + max);
+            for(int y=n-1;y>=0;y--)
+            {
+                for(int x = 0; x < n; x++)
+                {
+                    if (x >= y)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine(" ");
+            }
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("moi ban nhap so phan tu cua mang:");
-            int n = Convert.ToInt32(Console.ReadLine());
-            int[] arr = new int[n];
 
-            for (int i = 0; i < n; i++)
-            {
-                arr[i] = Convert.ToInt32(Console.ReadLine());
-            }
-            minimaxsum(arr);
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            staircase(n);
             Console.ReadLine();
         }
     }
